dbd996ebdb06f9aaafd3967de360df5246349a99
Maryna Hapon <marynahapon@gmail.com>
2020-03-27 21:08:57 +0200

    Updates

---
+++ precache-manifest.caddc8897ab44847feefefc2860d90d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a99b683ff92ad6e05243f5504abe2d8",
+    "url": "/list/index.html"
+  },
+  {
+    "revision": "4932949273242322e596",
+    "url": "/list/static/css/main.a6ca1756.chunk.css"
+  },
+  {
+    "revision": "39a405a3878e9b0a17aa",
+    "url": "/list/static/js/2.86b4bc42.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/list/static/js/2.86b4bc42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4932949273242322e596",
+    "url": "/list/static/js/main.fbabef9a.chunk.js"
+  },
+  {
+    "revision": "a938a87c4a6cbde36b6b",
+    "url": "/list/static/js/runtime-main.aae2ddb3.js"
+  },
+  {
+    "revision": "d37b8805e3562bc75949be91270529c9",
+    "url": "/list/static/media/logo.d37b8805.svg"
+  }
+]);
